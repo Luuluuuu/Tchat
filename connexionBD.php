a00687bec427162af6c37163588fcc61df8b4253
Luuluuuu <lucille4713@gmail.com>
2023-02-21 09:58:27 +0100

    connexionBD

---
+++ connexionBD.php
@@ -0,0 +1,22 @@
+<?php
+    function connexion(){
+        $server = "127.0.0.1";; 
+        $db = "projet_chat";
+        $login = "root";
+        $mdp = "";
+        try{
+            $linkpdo = new PDO("mysql:host=$server;dbname=$db",$login,$mdp);
+            return $linkpdo;
+        } catch (Exception $e){
+            die('Erreur : '.$e->getMessage());
+        }
+    }
+    
+    function deconnexion($linkpdo){
+        $linkpdo = null;
+    }
+
+    function hachage($string){
+            
+    }
+?>
